9a3740f604c12e5d79f9bfefa3f4888102eec273
Mekolaos <Mekolaos@gmail.com>
2020-04-09 17:17:50 +0100

    added real case and sample case to standard deviation

--- StandardDeviation/StandardDeviation.py
+++ StandardDeviation/StandardDeviation.py
@@ -1,17 +1,22 @@
 
 
 def mean(num_array):
+    array_len = len(num_array)
     num = 0
     for i in num_array:
         num += i
-    return int(num/len(num_array))
+    return int(num/array_len)
 
-def variance(num_array):
+def variance(num_array, array_len):
     mean_ = mean(num_array)
     num = 0
     for i in num_array:
         num += (i - mean_)**2
-    return int(num/len(num_array))
+    return int(num/array_len)
 
-def standard_deviation(num_array):
-    return int(variance(num_array) **0.5)
+def standard_deviation(num_array, sample):
+    if not sample:
+        return int(variance(num_array, len(num_array)) **0.5)
+    else:
+        return int(variance(num_array, len(num_array)-1) **0.5)
+        
--- StandardDeviation/test.py
+++ StandardDeviation/test.py
@@ -5,12 +5,21 @@ class MathTest(unittest.TestCase):
 
     def setUp(self):
         self.num_array = [600, 470, 170, 430, 300]
+        self.real_data_len = len(self.num_array)
+        self.sample_data_len = len(self.num_array)-1
     
     def test_mean(self):
         self.assertEqual(mean(self.num_array), 394)
     
     def test_variance(self):
-        self.assertEqual(variance(self.num_array), 21704)
+        # Real data case
+        self.assertEqual(variance(self.num_array, self.real_data_len), 21704)
+        # Sample data case
+        self.assertEqual(variance(self.num_array, self.sample_data_len), 27130)
+
 
     def test_standard_deviation(self):
-        self.assertEqual(standard_deviation(self.num_array), 147)
+        # Real data case
+        self.assertEqual(standard_deviation(self.num_array, False), 147)
+        # Sample data case 
+        self.assertEqual(standard_deviation(self.num_array, True), 164)
